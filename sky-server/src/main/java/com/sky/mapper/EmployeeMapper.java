fb8cb6f470d41d98a894fac18d48980bffa1568c
miku <1372813524@qq.com>
2024-09-25 10:55:38 +0800

    启用禁用员工账号

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,5 +1,7 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -21,4 +23,9 @@ public interface EmployeeMapper {
             "(id, username, name, password, phone, sex, id_number, status, create_time, update_time, create_user, update_user) " +
             "VALUES (#{id}, #{username}, #{name}, #{password}, #{phone}, #{sex}, #{idNumber}, #{status}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser});")
     void insert(Employee employee);
+
+
+    Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+
+    void update(Employee employee);
 }
